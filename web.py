865439b44679b63f0e3189f0802a5bb17bbf7971
IliaReykov <arhipiv1@gmail.com>
2025-03-19 14:12:34 +0400

    Fixed GET-request, function open() working correct.

--- web.py
+++ web.py
@@ -1,24 +1,36 @@
 # Импорт встроенной библиотеки для работы веб-сервера
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
+import os
 
 # Для начала определим настройки запуска
-hostName = "localhost" # Адрес для доступа по сети
-serverPort = 8080 # Порт для доступа по сети
+hostName = "localhost"  # Адрес для доступа по сети
+serverPort = 8080  # Порт для доступа по сети
+
 
 class MyServer(BaseHTTPRequestHandler):
     """
         Специальный класс, который отвечает за
         обработку входящих запросов от клиентов
     """
+
     def do_GET(self):
-        self.send_response(200) # Отправка кода ответа
-        self.send_header("Content-type", "text/html") # Отправка типа данных, который будет передаваться
-        self.end_headers() # Завершение формирования заголовков ответа
-        self.wfile.write(bytes("https://github.com/IliaReykov/dz_verstka/blob/master/contacts_1.html", "utf-8")) # Тело ответа
+        self.send_response(200)  # Отправка кода ответа
+        self.send_header("Content-type", "text/html")  # Отправка типа данных, который будет передаваться
+        self.end_headers()  # Завершение формирования заголовков ответа
+
+        # Правильный путь к файлу
+        file_path = os.path.join('contacts_1.html')
+
+        try:
+            with open(file_path, 'r', encoding='utf-8') as f:
+                content = f.read()
+            self.wfile.write(bytes(content, 'utf-8'))
+        except FileNotFoundError:
+            self.send_error(404, "File not found")  # Обработка ошибки, если файл не найден
+
 
 if __name__ == "__main__":
-    # Инициализация веб-сервера, который будет по заданным параметрах в сети
+    # Инициализация веб-сервера, который будет по заданным параметрам в сети
     # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
